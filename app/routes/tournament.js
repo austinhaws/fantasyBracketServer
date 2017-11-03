3807d8ee1ad524dfbfb56e8ba0033a0945330885
Austin Haws <ahaws@utah.gov>
2017-11-02 20:53:38 -0600

    Post save tournaments

--- app/routes/tournament.js
+++ app/routes/tournament.js
@@ -1,7 +1,14 @@
 const tournament = require('../models/tournament');
+const jsonMessage = require('../system/messages/jsonMessages');
 
 module.exports = function (router) {
 
-	router.route('/tournament/current').get((req, res) => tournament.select(2018, tournament => res.json(tournament)));
+	router.route('/tournament/current').get((req, res) => tournament.select(2018, tournaments => res.json(tournaments[0])));
+	router.route('/tournament/save').post((req, res) => tournament.update({
+			year: req.body.year,
+			data: req.body.data,
+			tournament_pk: req.body.tournamentPk,
+		}, () => jsonMessage.success(res)));
+
 
 };

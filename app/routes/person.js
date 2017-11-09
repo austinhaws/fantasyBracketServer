545f423137c763096af159e07a97ec3cbc42cb91
Austin Haws <ahaws@utah.gov>
2017-11-08 21:43:01 -0700

    Load picks from Mongo

--- app/routes/person.js
+++ app/routes/person.js
@@ -4,5 +4,6 @@ const authentication = require('../system/security/authentication');
 module.exports = function (router) {
 
 	router.route('/person/current').get((req, res) => authentication.currentUser(req, user => res.json(user)));
-	router.route('/person/picks').get((req, res) => authentication.currentUser(req, user => picks.select(user.uid, picks => res.json(picks.length ? JSON.parse(picks[0]) : '[]'))));
+
+	router.route('/person/picks').get((req, res) => authentication.currentUser(req, user => picks.select(user.uid, picks => res.json(picks.length ? picks[0] : '{}'))));
 };

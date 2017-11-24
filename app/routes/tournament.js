402432628ae15619a7d44af6a319c88895d54e93
Austin Haws <ahaws@utah.gov>
2017-11-23 20:35:16 -0700

    Save Game and Tournament
    These urls work for saving games and tournaments.

--- app/routes/tournament.js
+++ app/routes/tournament.js
@@ -5,5 +5,10 @@ module.exports = function (router) {
 
 	router.route('/tournament/current').get((req, res) => tournaments.select('2018', tournaments => res.json(tournaments[0])));
 
-	router.route('/tournament/save').post((req, res) => tournaments.update(req.body, () => jsonMessage.success(res)));
+	router.route('/tournament/save').post((req, res) =>
+		tournaments.select(req.body.year, tournamentRecords => {
+			// need ID as an object but all the other fields from the request
+			const tournament = Object.assign(tournamentRecords[0], req.body, {_id: tournamentRecords[0]._id});
+			tournaments.update(tournament, () => res.json(tournament))
+		}));
 };
